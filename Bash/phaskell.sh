b0946c8c597684fb8d5ad9c11d2887eb401056ea
Julian Lore <julian.lore@mail.mcgill.ca>
2019-02-17 19:36:14 -0500

    Added script to apply hindent and stylish-haskell to a file

---
+++ Bash/phaskell.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Prettify haskell with hindent and stylish-haskell
+if [ $# -lt 1 ]
+then
+    echo "Not enough arguments"
+    echo "Example usage: phaskell.sh filename"
+else
+    hindent $1
+    stylish-haskell -i $1
+fi
