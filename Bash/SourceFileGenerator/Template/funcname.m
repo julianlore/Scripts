27a4eba6c0df558f70aa0afc8312ae1f556c2a99
Julian Lore <julian.lore@mail.mcgill.ca>
2019-03-24 15:53:36 -0400

    Add Matlab function template

---
+++ Bash/SourceFileGenerator/Template/funcname.m
@@ -0,0 +1,6 @@
+function out = funcname(in)
+% funcname.m Function description
+% Input:     Input description
+%
+% Output:    Output description
+%
--- Bash/SourceFileGenerator/gen.sh
+++ Bash/SourceFileGenerator/gen.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 if [ $# -lt 2 ]
 then
-	echo "Not enough arguments"
-	echo "Usage: gen.sh lang source_file_name"
-	echo "Example: gen.sh bash new_script.sh"
+    echo "Not enough arguments"
+    echo "Usage: gen.sh lang source_file_name"
+    echo "Example: gen.sh bash new_script.sh"
 else
-	template_dir="`dirname "$0"`/Template"
-	case ${1^^} in
-		"BASH")
-			cp "$template_dir/script.sh" $2
-			;;
-		"BASH_ARG")
-			cp "$template_dir/script_arg.sh" $2
-			;;
-		"MAKE")
-			cp "$template_dir/Makefile" $2
-			;;
-		"PYTHON")
-			cp "$template_dir/template.py" $2
-			;;
-		*)
-			echo "No templates for source language $1"
-	esac
+    template_dir="`dirname "$0"`/Template"
+    case ${1^^} in
+	"BASH")
+	    cp "$template_dir/script.sh" $2
+	    ;;
+	"BASH_ARG")
+	    cp "$template_dir/script_arg.sh" $2
+	    ;;
+	"MAKE")
+	    cp "$template_dir/Makefile" $2
+	    ;;
+	"PYTHON")
+	    cp "$template_dir/template.py" $2
+	    ;;
+        "MATLAB")
+            cp "$template_dir/funcname.m" $2
+            file=$(basename -- "$2")
+            noext="${file%.*}"
+            sed -i -e "s/funcname/$noext/g" $2 # Replace occurrences of function with filename
+            ;;
+	*)
+	    echo "No templates for source language $1"
+    esac
 fi
